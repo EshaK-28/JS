8806e8ee144eeb65ad99a9f8a77429b19699d1f9
Esha Gopal Kumbhare <86103517+EshaK-28@users.noreply.github.com>
2024-02-09 06:56:08 +0000

    a1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 // console.log("esha")
 
-const a = 123
-var b = 'esha'
-let c = 2344
-d = "fghd"
+// const a = 123
+// var b = 'esha'
+// let c = 2344
+// d = "fghd"
 
 // We don't use var because of block scope and functional scope.
 
@@ -20,5 +20,30 @@ d = "fghd"
 // Undefined
 // Symbol - to find uniqueness, uniqueness
 
-console.log(typeof null);
-console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// when converting string to number.
+// "33" => 33
+// "33asd" = NaN
+// true = 1; false = 0;
+// let score = true;
+// let num = Number(score);
+// console.log(num);
+// console.log(typeof num);
+
+// converting string into boolean
+// "somestring" => true
+// "" => false
+// let str = "";
+// let val = Boolean(str);
+// console.log(val);
+// console.log(typeof val);
+
+// converting string into number
+
+// ****************************Operations***********************************
+
+let value = 3
+let negValue = -value
+console.log(value)
