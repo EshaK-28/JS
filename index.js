1510765872317179367339a1a96b5332af8a30f9
Esha Gopal Kumbhare <86103517+EshaK-28@users.noreply.github.com>
2024-02-09 06:35:05 +0000

    a1

---
+++ index.js
@@ -0,0 +1,24 @@
+// "use strict"; treat all js code as newer version
+
+// console.log("esha")
+
+const a = 123
+var b = 'esha'
+let c = 2344
+d = "fghd"
+
+// We don't use var because of block scope and functional scope.
+
+// console.table([a,b,c,d])
+// console.log(b)
+
+// Number - 2 to the power of 53
+// BigInt
+// String
+// Boolean
+// Null - standalone value
+// Undefined
+// Symbol - to find uniqueness, uniqueness
+
+console.log(typeof null);
+console.log(typeof undefined);
